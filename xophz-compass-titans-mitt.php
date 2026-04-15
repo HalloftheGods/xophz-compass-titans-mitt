038962e085e3d603ba4e96ca0b685891e32256d4
Xopher <xopher@vi.co>
2026-04-14 21:30:41 -0700

    chore: sync plugins

--- xophz-compass-titans-mitt.php
+++ xophz-compass-titans-mitt.php
@@ -19,7 +19,7 @@
  * Description:       Crush your site's largest objects or lift them into the cloud.
  * Version:           26.4.14.1311
  * Author:            Hall of the Gods, Inc.
- * Author URI:        http://www.midnightnerd.com/
+ * Author URI:        http://www.hallofthegods.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       xophz-compass-titans-mitt
